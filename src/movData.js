c696f2f4d625c08f400ef7c7e7d7aaf7bc2c031c
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-05-15 04:04:43 -0400

    Added delete and fixed duplicates

--- src/movData.js
+++ src/movData.js
@@ -6,7 +6,8 @@ export default [
         director: "Christopher Nolan",
         IMDbUrl: "https://www.imdb.com/title/tt0468569/",
         description: "When the menace known as the Joker wreaks havoc and chaos on the people of Gotham, Batman must accept one of the greatest psychological and physical tests of his ability to fight injustice.",
-        imageUrl: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg"
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+        actors: "Christian Bale, Heath Ledger, Aaron Eckhart"
     },
     {
         key: 2,
@@ -15,7 +16,8 @@ export default [
         director: "Frank Darabont",
         IMDbUrl: "https://www.imdb.com/title/tt0111161/",
         description: "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
-        imageUrl: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg"
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_FMjpg_UX1000_.jpg",
+        actors: "Tim Robbins, Morgan Freeman, Bob Gunton"
     },
     {
         key: 3,
@@ -24,7 +26,8 @@ export default [
         director: "David Fincher",
         IMDbUrl: "https://www.imdb.com/title/tt0114369/",
         description: "Two detectives, a rookie and a veteran, hunt a serial killer who uses the seven deadly sins as his motives.",
-        imageUrl: "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BOTUwODM5MTctZjczMi00OTk4LTg3NWUtNmVhMTAzNTNjYjcyXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg",
+        actors: "Morgan Freeman, Brad Pitt, Kevin Spacey"
     },
     {
         key: 4,
@@ -33,6 +36,7 @@ export default [
         director: "Gus Van Sant",
         IMDbUrl: "https://www.imdb.com/title/tt0119217/",
         description: "Will Hunting, a janitor at M.I.T., has a gift for mathematics, but needs help from a psychologist to find direction in his life.",
-        imageUrl: "https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg"
+        imageUrl: "https://m.media-amazon.com/images/M/MV5BOTI0MzcxMTYtZDVkMy00NjY1LTgyMTYtZmUxN2M3NmQ2NWJhXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_FMjpg_UX1000_.jpg",
+        actors: "Robin Williams, Matt Damon, Ben Affleck"
     }
 ]
