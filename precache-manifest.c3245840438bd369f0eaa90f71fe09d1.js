67a4ba08eb19e4f7d9c531144474575bc9da11ad
mohammadnayeem1 <mohammadnayeem2000@gmail.com>
2022-06-22 13:50:28 -0400

    Updates

---
+++ precache-manifest.c3245840438bd369f0eaa90f71fe09d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f11faba228ab5ce69231",
+    "url": "/Movies-To-Watch-App/static/css/main.b3113a20.chunk.css"
+  },
+  {
+    "revision": "f11faba228ab5ce69231",
+    "url": "/Movies-To-Watch-App/static/js/main.f11faba2.chunk.js"
+  },
+  {
+    "revision": "efdaaebbc5ad3ba623fb",
+    "url": "/Movies-To-Watch-App/static/js/1.efdaaebb.chunk.js"
+  },
+  {
+    "revision": "e1aec82cb82b9b899101",
+    "url": "/Movies-To-Watch-App/static/js/runtime~main.e1aec82c.js"
+  },
+  {
+    "revision": "289458c87ad0cf3000751ea0643ff365",
+    "url": "/Movies-To-Watch-App/static/media/film.289458c8.png"
+  },
+  {
+    "revision": "2f652b730e04310d2687e1ed25270691",
+    "url": "/Movies-To-Watch-App/index.html"
+  }
+];
